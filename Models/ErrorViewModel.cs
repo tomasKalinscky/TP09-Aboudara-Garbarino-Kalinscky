b1908e66f2d57099faa0816042b027e8bfdd9525
TOMÁS KALINSCKY <47494132@ort>
2023-09-28 14:25:00 -0300

    creado el proyecto. Creados los controlers, views y models. Creada la funcion validarContraseña del js

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Aboudara_Garbarino_Kalinscky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿function validarContraseña(contra) {
+    let countCaracteres = 0;
+    const caracteresMin = 8;
+    let especial = false;
+    let mayus = false;
+    while (countCaracteres < contra.length) {
+        mayus = contra[countCaracteres] === contra[countCaracteres].toUpperCase() && contra[countCaracteres] !== contra[countCaracteres].toLowerCase();
+        especial = /[^a-zA-Z0-9\s]/.test(contra[countCaracteres]);
+        if (countCaracteres >= caracteresMin && especial && mayus) return true;
+        countCaracteres++;
+    }
+    return false;
+}
+
+
+
