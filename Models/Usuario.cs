41e057eaba9860b3328350484250f4024aeaf96c
MATÍAS ABOUDARA <47701750@ort>
2023-09-28 15:35:21 -0300

    fsad

---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace TP7_Aboudara_Garbarino_Kalinscky.Models;
+public class Categoria{
+    public int IDUsuario;
+    public string Contraseña;
+    public string UserName;
+    public string? Nombre;
+    public string? Email;
+    public string? telefono; 
+}
