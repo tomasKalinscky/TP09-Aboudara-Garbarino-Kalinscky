b8ff3440b1fab409a9cd3b4af16edab84d00554e
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-03 07:59:08 -0300

    safasd

---
+++ Models/BD.cs
@@ -0,0 +1,14 @@
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+namespace TP09_Aboudara_Garbarino_Kalinscky.Models;
+
+public static class BD{
+
+    private static string _connectionString = @"Server=localhost; DataBase=base_de_datos-TP09";
+
+    
+
+
+}
