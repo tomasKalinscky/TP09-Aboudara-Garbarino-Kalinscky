b9822e8074700a3b7332fbf2343bd7fd92001fab
TOMÁS KALINSCKY <47494132@ort>
2023-10-05 15:11:33 -0300

    anashe

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,5 @@
 namespace TP7_Aboudara_Garbarino_Kalinscky.Models;
 public class Usuario{
-    public int IDUsuario;
     public string Contraseña;
     public string UserName;
     public string? Nombre;
@@ -8,7 +7,7 @@ public class Usuario{
     public string? Telefono; 
 }
 
-public Usuario(string UserName = "", string Contraseña = "", Nombre = "", Email = "", Telefono = "") {
+public Usuario(string UserName = "", string Contraseña = "", string Nombre = "", string Email = "", string Telefono = "") {
     this.UserName = UserName;
     this.Contraseña = Contraseña;
     this.Nombre = Nombre;
