b30061fd954bfaacc8ffbd6ff7966b48f9333951
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-05 13:51:43 -0300

    fsaasf

--- Controllers/account.cs
+++ Controllers/account.cs
@@ -1,16 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TP09_Aboudara_Garbarino_Kalinscky.Models;
-
+public class account : Controller
+{
+    private readonly ILogger<account> _logger;
+    
     public IActionResult registro() {
-
+        return View("Index");
     }
 
     public IActionResult olvide() {
-
+        return View("olvide");
     }
 
     public IActionResult login() {
-        
+        return View("Index");
     }
-    
+}
